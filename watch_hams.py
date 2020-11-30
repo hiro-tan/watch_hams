17c32161770d030cc2ea8bd0f9e5477386604317
hirota_yohei <y.hirota826@gmail.com>
2020-12-01 01:56:16 +0900

    replace adafruit_dht library

--- watch_hams.py
+++ watch_hams.py
@@ -2,7 +2,8 @@
 
 import os
 from datetime import datetime
-import Adafruit_DHT
+import adafruit_dht
+from board import D18
 import gspread
 
 from linebot import LineBotApi
@@ -11,8 +12,7 @@ from linebot.models import TextSendMessage
 spreadsheet_id = os.getenv('SPREADSHEET_ID')
 RECORD_COUNT = 720
 
-DHT_TYPE = Adafruit_DHT.DHT22
-DHT_PIN = 4
+dht_device = adafruit_dht.DHT22(D18)
 ALERT_TEMP_MIN = 20
 ALERT_TEMP_MAX = 26
 ALERT_INTERVAL = 10
@@ -74,7 +74,8 @@ def main():
         delete_old_data(sheet)
         column_num -= 1
 
-    humidity, temp = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
+    temp = dht_device.temperature
+    humidity = dht_device.humidity
     # 温度が適温でない時はLINEでpush通知を送る
     # ただし直前の温度も適温でなかった時は連続では通知を送らない
     # ALERT_INTERVAL の間適温でない状態が続いた時は追加の通知を送る
